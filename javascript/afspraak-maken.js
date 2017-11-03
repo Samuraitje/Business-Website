7c340778ab88833f8f03d41be2516573e34c6d16
Jeroen Huisman <jphuisman92@gmail.com>
2017-11-03 16:38:12 +0100

    171102-Afspraak-maken-4

--- javascript/afspraak-maken.js
+++ javascript/afspraak-maken.js
@@ -31,6 +31,21 @@ $(document).ready(function(){
 	})
 
 
+	$( ".cross" ).hide();
+	$( ".menu" ).hide();
+	$( ".hamburger" ).click(function() {
+	$( ".menu" ).slideToggle( "slow", function() {
+	$( ".hamburger" ).hide();
+	$( ".cross" ).show();
+	});
+	});
+
+	$( ".cross" ).click(function() {
+	$( ".menu" ).slideToggle( "slow", function() {
+	$( ".cross" ).hide();
+	$( ".hamburger" ).show();
+	});
+	});
 
 
 
